7be2d748083323078f03dfa7ed9bd39ef1477e5c
Fahadiya Binsy <130633503+fahadiya@users.noreply.github.com>
2024-11-10 13:53:29 +0530

    CALCULATOR

---
+++ CALCULATOR/script.js
@@ -0,0 +1,12 @@
+function btnclick(val){
+    console.log(val)
+    document.getElementById("screen").value+=val
+}
+function clearScreen(){
+    document.getElementById("screen").value=""
+}
+function equalclick(){
+    var text=document.getElementById("screen").value
+    var result=eval(text)
+    document.getElementById('screen').value=result
+}
